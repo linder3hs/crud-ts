93f0978953af02113548a261de887d30567468ed
Linder Hassinger <linder@peppermint.pe>
2024-01-12 21:10:55 -0500

    fix: change outDir

--- dist/src/crypto/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.hash = void 0;
-const node_crypto_1 = require("node:crypto");
-function hash(text) {
-    return (0, node_crypto_1.createHmac)("sha256", "secret").update(text).digest("hex");
-}
-exports.hash = hash;
